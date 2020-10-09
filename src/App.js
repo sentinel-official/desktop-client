37fcf34866d0a0a7e56bd5aad3b68eda261e69d9
Adwait Raste <adwait.raste@research.iiit.ac.in>
2020-10-09 18:50:04 +0530

    Added loading page to index.html without a progress bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import React from 'react';
 import './App.css';
 
-const App = () => {
-    return (
-        <div className="App">
-        </div>
-    );
-};
+class App extends React.Component {
+    state = {
+        loading: true
+    };
+
+    componentDidMount() {
+        const loader = document.querySelector(".loader-container");
+        if (loader) {
+            loader.remove();
+            this.setState({ loading: false });
+        }
+    }
+
+
+    // Use the Below commented code to test the loading screen. Before doing so comment out the componentDidMount method above
+
+    // componentDidMount() {
+    //     this.fakeRequest().then(() => {
+    //         const el = document.querySelector(".loader-container");
+    //         if (el) {
+    //             el.remove();  // removing the spinner element
+    //             this.setState({ loading: false }); // showing the app
+    //         }
+    //     });
+    // }
+
+    // fakeRequest = () => {
+    //     return new Promise(resolve => setTimeout(() => resolve(), 2500));
+    // };
+
+
+    render() {
+        if (this.state.loading)
+            return null;
+
+        return (
+            <div className="App">
+            </div>
+        );
+    }
+}
+
+
+
+
+// const App = () => {
+//     return (
+//         <div className="App">
+//         </div>
+//     );
+// };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <App />
     </React.StrictMode>,
     document.getElementById('root'),
 );
