c6b340ab474c06e7f2d4b5145e9954d4f4657cd2
Srinivas Baride <srini@exidio.co>
2020-11-04 22:10:10 +0530

    Added key handlers

---
+++ cli/handlers/keys/handlers.go
@@ -0,0 +1,132 @@
+package keys
+
+import (
+	"net/http"
+
+	"github.com/cosmos/cosmos-sdk/client/keys"
+	"github.com/cosmos/go-bip39"
+	"github.com/gorilla/mux"
+	"github.com/spf13/viper"
+
+	"github.com/sentinel-official/desktop-client/cli/models"
+	"github.com/sentinel-official/desktop-client/cli/types"
+	"github.com/sentinel-official/desktop-client/cli/utils"
+)
+
+func HandlerGetKeys(_ *types.Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		kb, err := keys.NewKeyBaseFromDir(viper.GetString(types.FlagHome))
+		if err != nil {
+			utils.WriteErrorToResponse(w, 500, 1, err.Error())
+			return
+		}
+
+		infos, err := kb.List()
+		if err != nil {
+			utils.WriteErrorToResponse(w, 500, 2, err.Error())
+			return
+		}
+
+		items := models.NewKeysFromRaw(infos, make([]string, len(infos)))
+		utils.WriteResultToResponse(w, 200, items)
+	}
+}
+
+func HandlerGetKey(_ *types.Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		kb, err := keys.NewKeyBaseFromDir(viper.GetString(types.FlagHome))
+		if err != nil {
+			utils.WriteErrorToResponse(w, 500, 1, err.Error())
+			return
+		}
+
+		vars := mux.Vars(r)
+
+		info, err := kb.Get(vars["name"])
+		if err != nil {
+			utils.WriteErrorToResponse(w, 500, 2, err.Error())
+			return
+		}
+
+		item := models.NewKeyFromRaw(info, "")
+		utils.WriteResultToResponse(w, 200, item)
+	}
+}
+
+func HandlerAddKey(_ *types.Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		body, err := NewRequestAddKey(r)
+		if err != nil {
+			utils.WriteErrorToResponse(w, 400, 1, err.Error())
+			return
+		}
+
+		if err = body.Validate(); err != nil {
+			utils.WriteErrorToResponse(w, 400, 2, err.Error())
+			return
+		}
+
+		kb, err := keys.NewKeyBaseFromDir(viper.GetString(types.FlagHome))
+		if err != nil {
+			utils.WriteErrorToResponse(w, 500, 3, err.Error())
+			return
+		}
+
+		info, _ := kb.Get(body.Name)
+		if info != nil {
+			utils.WriteErrorToResponse(w, 400, 4, "duplicate key name")
+			return
+		}
+
+		if body.Mnemonic == "" {
+			entropy, err := bip39.NewEntropy(256)
+			if err != nil {
+				utils.WriteErrorToResponse(w, 400, 5, err.Error())
+				return
+			}
+
+			body.Mnemonic, err = bip39.NewMnemonic(entropy)
+			if err != nil {
+				utils.WriteErrorToResponse(w, 400, 6, err.Error())
+				return
+			}
+		}
+
+		info, err = kb.CreateAccount(body.Name, body.Mnemonic, body.BIP39Password, body.Password, 0, 0)
+		if err != nil {
+			utils.WriteErrorToResponse(w, 500, 7, err.Error())
+			return
+		}
+
+		item := models.NewKeyFromRaw(info, body.Mnemonic)
+		utils.WriteResultToResponse(w, 201, item)
+	}
+}
+
+func HandlerDeleteKey(_ *types.Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		body, err := NewRequestDeleteKey(r)
+		if err != nil {
+			utils.WriteErrorToResponse(w, 400, 1, err.Error())
+			return
+		}
+
+		if err = body.Validate(); err != nil {
+			utils.WriteErrorToResponse(w, 400, 2, err.Error())
+			return
+		}
+
+		kb, err := keys.NewKeyBaseFromDir(viper.GetString(types.FlagHome))
+		if err != nil {
+			utils.WriteErrorToResponse(w, 500, 3, err.Error())
+			return
+		}
+
+		if err = kb.Delete(body.Name, body.Password, false); err != nil {
+			utils.WriteErrorToResponse(w, 500, 4, err.Error())
+			return
+		}
+
+		utils.WriteResultToResponse(w, 200, nil)
+	}
+}
